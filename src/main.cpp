2d32a0834381d0186cbf70deec39c640e85995ee
D0746265 <paul776618@gmail.com>
2019-11-14 20:04:28 +0800

    Fixed

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int addNumber(int a, int b) {
 }
 
 int getTriangleType(int a, int b, int c) {
-    if (a > b && a > c) {555555555
+    if (a > b && a > c) {
         int temp = c; c = a; a = temp;
     }
     else if (b > a && b > c) {
